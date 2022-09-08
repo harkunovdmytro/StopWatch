6d7250d789a9f58af372000ba3d5163322df9d31
harkunovdmytro <harkundmytro@gmail.com>
2022-09-08 12:25:33 +0300

    added reset button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { AppComponent } from './app.component';
 import { StopWatchComponent } from './stop-watch/stop-watch.component';
-
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { MatButtonModule } from '@angular/material/button';
-import {MatTooltipModule} from '@angular/material/tooltip';
+import { NumToTimePipe } from './pipes/numToTimePipe.pipe';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     StopWatchComponent,
+    NumToTimePipe
   ],
   imports: [
     BrowserModule,
@@ -21,7 +20,6 @@ import {MatTooltipModule} from '@angular/material/tooltip';
     MatButtonModule,
     MatTooltipModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/stop-watch/stop-watch.component.ts
+++ src/app/stop-watch/stop-watch.component.ts
@@ -4,39 +4,37 @@ import { timer, Subject, takeUntil, debounceTime, buffer, filter, map, Observabl
 @Component({
   selector: 'app-stop-watch',
   templateUrl: './stop-watch.component.html',
-  styleUrls: ['./stop-watch.component.scss']
+  styleUrls: ['./stop-watch.component.scss'],
 })
 export class StopWatchComponent implements OnInit {
   time = 0;
-  dat = new Date();
 
   isStarted = false;
   isPaused = false;
 
-  private observer = new Subject();
+  private observer$: Subject<{}> = new Subject();
   private rxjsTimer$ = timer(0, 1000);
 
   private delay: number = 0;
-  private subscription$!: Observable<number>;
   private waitTrigger = new Subject();
 
   ngOnInit(): void {
-    this.dat.setHours(0, 0, 0, 0);
     const clickStream = this.waitTrigger.asObservable();
     const clickDelay = 300;
 
     clickStream.pipe(
       buffer(clickStream.pipe(debounceTime(clickDelay))),
       map(list => list.length),
-      filter((x) => x >= 2)
-    ).subscribe(
-      (): void => {
-        this.delay = this.time;
-        this.stopIteartion();
-        this.setTime(this.delay)
-        this.isPaused = true;
-      }
-    )
+      filter((x) => x >= 2),
+    ).subscribe((): void => {
+      this.delay = this.time;
+      this.stopIteartion();
+      this.isPaused = true;
+    });
+  }
+
+  onDestroy() {
+    this.stopIteartion();
   }
 
   waitTimer(): void {
@@ -47,33 +45,30 @@ export class StopWatchComponent implements OnInit {
     this.isStarted = true;
     this.isPaused = false;
 
-    this.subscription$ = this.rxjsTimer$.pipe(takeUntil(this.observer))
-    this.subscription$.subscribe(
-      (counter) => {
-        this.time = counter + this.delay;
-        this.setTime(this.time)
-      })
+    this.rxjsTimer$.pipe(
+      takeUntil(this.observer$),
+    ).subscribe((counter) => {
+      this.time = counter + this.delay;
+    })
   }
   stopTimer(): void {
     this.isStarted = false;
     this.isPaused = false;
 
-    this.stopIteartion()
+    this.stopIteartion();
     this.time = 0;
     this.delay = 0;
-    this.dat.setHours(0, 0, 0, 0);
+  }
+
+  resetTimer() {
+    this.stopTimer();
+    this.startTimer();
   }
 
   private stopIteartion(): void {
-    this.observer.next('');
-    this.observer.complete();
-    this.observer = new Subject();
-    this.setTime()
-
-  }
-  private setTime(time: number = 0) {
-    this.dat = new Date()
-    this.dat.setHours(0, 0, time);
+    this.observer$.next('');
+    this.observer$.complete();
+    this.observer$ = new Subject();
   }
 
 }
